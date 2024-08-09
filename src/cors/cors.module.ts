f602900af23b2b195334ec46cbaaaaae1b695201
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-08-09 18:54:17 +0530

    nothing to change

--- src/cors/cors.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CorsInterceptor } from './cors.interceptor';
-import { APP_INTERCEPTOR } from '@nestjs/core';
-
-@Module({})
-export class CorsModule {
-    providers: CorsInterceptor
-}
