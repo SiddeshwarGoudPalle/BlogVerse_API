ab95867a550c7d9feb15f521301b01838622f556
SIDDESHWAR369 <ugs21052_cic.siddeshwar@cbit.org.in>
2024-08-11 12:39:11 +0530

    vercel.json

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
    app.enableCors();
-
-  
-
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
